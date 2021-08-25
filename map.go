64e028f3da4abef55a223803a9cc206b3e7d0255
Rajan Prajapati <rajanprajapati1199@gmail.com>
2021-08-25 12:45:33 +0530

    map santax

--- map.go
+++ map.go
@@ -7,6 +7,10 @@ func main() {
 	m["rajan"] = 200
 	m["kumar"] = 100
 	fmt.Println(m)
+   m["vikash"]=2000
+   m["sandeep"]=3000
+
+   fmt.Println("sandeep ", m["sandeep"]);//print value of any key
 
 	a:=map[string]int{"a":2,}
 	fmt.Print(a)
